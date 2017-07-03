a6649b19441bc18af3c8cb2a864071b3868f0c7f
Neil Goldman <neilgoldman305@gmail.com>
2017-07-02 20:55:37 -0400

    initial commit

---
+++ GameController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+using Random = UnityEngine.Random;
+
+public class GameController : MonoBehaviour
+{
+
+	public GameObject hazardsManager;
+	
+	public Vector3 spawnValues;
+//	public int hazardCount;
+//	public float startWait;
+//	public float spawnWait;
+//	public float waveWait;
+
+
+	public float ScrollSpeed;
+	
+	
+	public GameObject FrontLayer;
+	public GameObject ParallaxLayerFast;
+	public GameObject ParallaxLayerSlow;
+	public GameObject Background;
+
+//	public GameObject Turtle;
+//	private List<GameObject> Hazards;
+
+	public GUIText scoreText;
+	public GUIText restartText;
+	public GUIText gameOverText;
+
+	private bool gameOver;
+	private bool restart;
+	public int score;
+
+	// Use this for initialization
+	void Start ()
+	{
+		gameOver = false;
+		restart = false;
+		restartText.text = "";
+		gameOverText.text = "";
+		score = 0;
+		this.UpdateScore();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (restart)
+		{
+			if (Input.GetKeyDown(KeyCode.R))
+			{
+				Application.LoadLevel(Application.loadedLevel);
+			}
+		}
+
+		//Front Layer
+		foreach (Transform child in FrontLayer.transform)
+		{
+			child.position += Vector3.left * ScrollSpeed * Time.smoothDeltaTime;
+		}
+
+		//Parallax Fast Layer
+		foreach (Transform child in ParallaxLayerFast.transform)
+		{
+			child.position += (Vector3.left * ScrollSpeed * Time.smoothDeltaTime) * 0.6f;
+		}
+
+		//Parallax Slow Layer
+		foreach (Transform child in ParallaxLayerSlow.transform)
+		{
+			child.position += (Vector3.left * ScrollSpeed * Time.smoothDeltaTime) * 0.2f;
+		}
+
+		// Only if we can get a horizontally seamless background	
+//		Background.transform.position += (Vector3.left * ScrollSpeed) * 0.1f;
+	}
+
+	public void AddScore(int newScoreValue)
+	{
+		score += newScoreValue;
+		this.UpdateScore();
+	}
+
+	void UpdateScore()
+	{
+		scoreText.text = "Score: " + score;
+	}
+
+	public void GameOver()
+	{
+		this.gameOverText.text = "Game Over";
+		gameOver = true;
+	}
+}
---
+++ PlayerController.cs
@@ -0,0 +1,146 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+[System.Serializable]
+public class Boundary
+{
+	public float xMin, xMax, yMin, yMax;
+}
+	 
+
+public class PlayerController : MonoBehaviour {
+    public GameObject[] hazards;
+    public Vector3 spawnValues;
+    public int hazardCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+
+    public GUIText scoreText;
+    public GUIText restartText;
+    public GUIText gameOverText;
+
+    private bool gameOver;
+    private bool restart;
+    private int score;
+
+
+
+    public float speed;
+	public Boundary boundary = new Boundary();
+	public float tilt;
+
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+    public CNAbstractController MovementJoystick;
+    public void Shoot()
+    {
+        nextFire = Time.time + fireRate;
+        Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+        audio.Play();
+    }
+
+	private float nextFire;
+
+	void Update()
+	{
+        //if (Input.GetButton("Fire1") && Time.time > nextFire)
+        //{
+        
+        //}
+
+        float moveHorizontal = MovementJoystick.GetAxis("Horizontal");
+        float moveVertical = MovementJoystick.GetAxis("Vertical");
+        
+
+        Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0.0f);
+        rigidbody.velocity = Vector3.Lerp(movement * speed * Time.smoothDeltaTime, rigidbody.velocity, 0.3f);
+
+        rigidbody.position = new Vector3(
+            Mathf.Clamp(rigidbody.position.x, boundary.xMin, boundary.xMax),
+            Mathf.Clamp(rigidbody.position.y, boundary.yMin, boundary.yMax),
+            0.0f
+        );
+
+        rigidbody.rotation = Quaternion.Lerp(
+            Quaternion.Euler(((rigidbody.velocity.y * 4.0f - rigidbody.velocity.x) * tilt) + 2.0f, 90.0f, -100.0f),
+            rigidbody.rotation,
+            0.2f
+        );
+    }
+
+	void FixedUpdate()
+	{
+		
+	}
+    void Start()
+    {
+        gameOver = false;
+        restart = false;
+        restartText.text = "";
+        gameOverText.text = "";
+        score = 0;
+        UpdateScore();
+        StartCoroutine(SpawnWaves());
+    }
+
+    void Pablo()
+    {
+        if (restart)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                Application.LoadLevel(Application.loadedLevel);
+            }
+        }
+    }
+
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            for (int i = 0; i < hazardCount; i++)
+            {
+                GameObject hazard = hazards[Random.Range(0, hazards.Length)];
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+
+            if (gameOver)
+            {
+                restartText.text = "Press 'R' for Restart";
+                restart = true;
+                break;
+            }
+        }
+    }
+
+    public void AddScore(int newScoreValue)
+    {
+        score += newScoreValue;
+        UpdateScore();
+    }
+
+    void UpdateScore()
+    {
+        scoreText.text = "Score: " + score;
+    }
+
+    public void GameOver()
+    {
+        gameOverText.text = "Game Over!";
+        gameOver = true;
+    }
+
+    public static implicit operator PlayerController(PlayerController v)
+    {
+        throw new NotImplementedException();
+    }
+}
+
